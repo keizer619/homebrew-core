ace0b8ae4cd58c5e114352d080d1c61ca6e7a082
Tharik Kanaka <tharik.kanaka@gmail.com>
2021-03-03 23:27:22 +0530

    Migrate ballerina to swan lake releases

--- Formula/ballerina.rb
+++ Formula/ballerina.rb
@@ -1,8 +1,8 @@
 class Ballerina < Formula
   desc "Programming Language for Network Distributed Applications"
   homepage "https://ballerina.io"
-  url "https://dist.ballerina.io/downloads/1.2.13/ballerina-1.2.13.zip"
-  sha256 "ba2b6cbf09f5129a72afa3f494da5c7304d9321b32c4a1504c5a2b11644c2c57"
+  url "https://dist.ballerina.io/downloads/swan-lake-alpha2/ballerina-swan-lake-alpha2.zip"
+  sha256 "17c8c2101d18d1058132a102d5f3e7d5536a0da503384f6ddc14318ac864fdf4"
   license "Apache-2.0"
 
   livecheck do
@@ -12,17 +12,17 @@ class Ballerina < Formula
 
   bottle :unneeded
 
-  depends_on "openjdk@8"
+  depends_on "openjdk@11"
 
   def install
     # Remove Windows files
     rm Dir["bin/*.bat"]
 
-    chmod 0755, "bin/ballerina"
+    chmod 0755, "bin/bal"
 
-    bin.install "bin/ballerina"
+    bin.install "bin/bal"
     libexec.install Dir["*"]
-    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("11"))
   end
 
   test do
@@ -32,7 +32,7 @@ class Ballerina < Formula
         io:println("Hello, World!");
       }
     EOS
-    output = shell_output("#{bin}/ballerina run helloWorld.bal")
+    output = shell_output("#{bin}/bal run helloWorld.bal")
     assert_equal "Hello, World!", output.chomp
   end
 end
